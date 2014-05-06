9fb39abe83bb248d1f879ea9db5649570c0da495
Emma Guy <emmag87@gmail.com>
2014-05-06 19:45:09 +0100

    added basic afterSave function to add a Message if the PendingMessage is to a person we know about (by email addr), and clean up PM after

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,33 @@
+Parse.Cloud.afterSave("PendingMessage", function(request) {
+	var toEmail = request.object.get("toEmail");
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  	query = new Parse.Query("User");
+  	query.equalTo("email", toEmail);
+  	query.first({
+      success: function(user) {
+        if (user) {
+			var Message = Parse.Object.extend("Message");
+
+			var message = new Message();
+			message.set("toUser", user);
+			message.set("fromUser", request.object.get("fromUser"));
+			message.set("image", request.object.get("image"));
+			message.save();
+
+			// could do this in beforeSave instead, but then we have to return error when a User exists
+
+			request.object.destroy({
+			  success: function(pendingmsg) {
+			    console.log("Successfully deleted PendingMessage after ");
+			  },
+			  error: function(pendingmsg, error) {
+			    console.error("Failed to delete PendingMessage after saving it as a Message, error code:" + error.code + " : " + error.message);
+			  }
+			});
+    	}
+    },
+    error: function(error) {
+      console.error("Failed to retrieve users with email: " + toEmail + ", error code:" + error.code + " : " + error.message);
+    }
+  });
 });
